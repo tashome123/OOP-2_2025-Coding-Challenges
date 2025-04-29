18271c3ff90c4b7e673b20d54047faf92dde2aef
tashome123 <tashome.makonnen@strathmore.edu>
2025-04-29 13:13:17 +0300

    Add files via upload

---
+++ Challenge2_Use_Of_Variable.java
@@ -0,0 +1,8 @@
+package Week_1;
+
+public class Challenge2_Use_Of_Variable {
+    public static void main(String[] args) {
+        String greeting = "Hello";
+        System.out.println(greeting);
+    }
+}
