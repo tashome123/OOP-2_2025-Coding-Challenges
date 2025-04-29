5179b6fc65d634399be97845b95d901c7838a2a2
tashome123 <tashome.makonnen@strathmore.edu>
2025-04-29 13:13:32 +0300

    Add files via upload

---
+++ Challenge3_Use_Of_If_Else.java
@@ -0,0 +1,16 @@
+
+package Week_1;
+
+public class Challenge3_Use_Of_If_Else {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 7;
+        if (num1 > num2) {
+            System.out.println(num1 + " is greater than " + num2);
+        } else if (num1 < num2) {
+            System.out.println(num1 + " is less than " + num2);
+        } else if (num1 == num2) {
+            System.out.println(num1 + " is equal to " + num2);
+        }
+    }
+}
