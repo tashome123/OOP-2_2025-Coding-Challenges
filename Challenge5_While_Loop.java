f8468160bc71d06710fa0464ca1e8a189b7af78c
tashome123 <tashome.makonnen@strathmore.edu>
2025-04-29 13:14:02 +0300

    Add files via upload

---
+++ Challenge5_While_Loop.java
@@ -0,0 +1,11 @@
+package Week_1;
+
+public class Challenge5_While_Loop {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 10) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
