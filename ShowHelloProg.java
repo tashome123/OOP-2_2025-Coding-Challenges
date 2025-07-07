5628a9d6dac45398655fe6f4ceb641a63dd66c01
tashome123 <tashome.makonnen@strathmore.edu>
2025-07-07 11:58:34 +0300

    Add files via upload

---
+++ ShowHelloProg.java
@@ -0,0 +1,8 @@
+package Challenges;
+public class ShowHelloProg {
+    public static void main (String args[]){
+        HelloUntilStop h1 = new HelloUntilStop();
+
+        h1.showHello();
+    }
+}
